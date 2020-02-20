b16ac8b422942b737d42a942b3fb47cdea0823f4
Marina <alcubilla.marina@gmail.com>
2020-02-19 20:28:41 -0600

    antes de hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import FormContainer from "./containers/FormContainer"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./components/Header";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App=()=> {
-  return (
+return (
+
+  <Router>
+    <Header />
     <div className="container">
-      <FormContainer />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/about/:name" component={About} />
+      </Switch>
     </div>
-  );
+
+  </Router>
+);
 }
 
 export default App;
